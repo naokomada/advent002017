2cfd248a3497cfb030575eb77ea376dbdc2648fd
komada naoki <komada.naoki@shanon.co.jp>
2017-12-05 21:05:51 +0900

    first commit

---
+++ app/src/main/java/jp/co/shanon/advend2017/MainActivity.kt
@@ -0,0 +1,31 @@
+package jp.co.shanon.advend2017
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+//
+//public class TestMainActivity extends AppCompatActivity implements SampleFragment.OnFragmentInteractionListener {
+//
+//    // something
+//    @Override
+//    public void onFragmentInteraction(Uri uri) {
+//
+//    }
+//
+//}
+class TestMainActivity : AppCompatActivity(), SampleFragment.OnFragmentInteractionListener {
+
+    // something
+    fun onFragmentInteraction(uri: Uri) {
+
+    }
+
+}
